93e4f49bf6abdea45442425594c4f9fda8548ba9
monaSalih <monaalobisat@gmail.com>
2021-10-12 15:26:31 +0300

    new docString

--- python/code_challenges/array-insert-shift1/array_insert_shift1/array_insert_shift1.py
+++ python/code_challenges/array-insert-shift1/array_insert_shift1/array_insert_shift1.py
@@ -2,6 +2,11 @@ import math
 #[2,4,6,-8] insert 5
 #[2,4,5,6,-6]
 def insertShiftArray(arr,x):
+    """
+    this code to insert new number in the middle of the array
+    input=>#[2,4,6,-8] insert 5
+    output=>[2,4,5,6,-6]
+    """
     z=0
     for i in arr:
             z+=1    #10
