5937a374dc65ce03e38ddae14a11371c8b120c56
monaSalih <monaalobisat@gmail.com>
2021-10-16 21:10:49 +0300

    new update

--- python/code_challenges/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list.py
@@ -5,9 +5,10 @@ class Node:
 
   """
 
-  def __init__(self, data, next_ = None):
+  def __init__(self, data, next_=None):
     self.data = data
     self.next = next_
+
 
 class LinkedList:
   """
@@ -28,38 +29,37 @@ class LinkedList:
     """
     self.head = Node(value, self.head)
 
-  def includes(self,value ):
-      current=self.head
-      while current != None:
-          if current.data==value:
-              return True
-          else:
-              current=current.next
-      return False
-
-    #    """"
-    #    input: 1 value
-    #    output:true
-    #    action:to check if the value exist in the
-    #    """
-  
+  def includes(self, value):
+    """
+    input: 1 value
+    output:true
+    action:to check if the value exist in the
+    """
+    current = self.head
+    while current != None:
+      if current.data == value:
+        return True
+      else:
+        current = current.next
+    return False
 
   def toString(self):
-      input_none = ""
-      current = self.head
-      while current:
-          data = current.data
-          if current.next is None:
-              input_none  +="{"+f'{data}'+"}->Null"
-              break
-          else:
-              input_none  +="{"+f' {data}'+"}-> "
-              data = current.next
-      return input_none
-    #   """
-    #   input:none
-    #   output:{ a } -> { b } -> { c } -> NULL
-    #   action:return formated string represent list value
-    #   """
+     """
+     input:none
+     output:{ a } -> { b } -> { c } -> NULL
+     action:return formated string represent list value
+     """
+     input_none = ""
+     current = self.head
+     while current:
+       data = current.data
+       if current.next is None:
+         input_none += "{"+f'{data}'+"}->Null"
+         break
+       else:
+         input_none  +="{"+f' {data}'+"}-> "
+         data = current.next
+     return input_none
+
 
 
