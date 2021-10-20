403f4f4e0a3f10eb14f81f1afd1cb5c1b3f2408f
monaSalih <monaalobisat@gmail.com>
2021-10-20 22:50:41 +0300

    update

--- python/code_challenges/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list.py
@@ -129,6 +129,21 @@ class LinkedList:
              else:
                 counter +=1
                 current=current.next
+# ================================================
+#   def palindrome(self):
+#     node = self.head
+#     length = 0
+#     comp = []
+#     while node:
+#       comp.append(node.data)
+#       node = node.next
+#       length += 1
+#     node = self.head
+#     for item in comp[::-1]:
+#       if (item!= node.data):
+#         return False
+#       node = node.next
+#     return True
 
 # ================================================
 def ziplist(list1,list2):
